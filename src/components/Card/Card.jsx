a26b7388cb19b0ab76f36166abccec80a95fc486
Andrii <Popov.Andrey80@gmail.com>
2023-05-08 23:48:23 +0300

    add Card styles

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -3,16 +3,25 @@ import boy from "../../css/img/boy2x.jpeg";
 import sigths from "../../css/img/picture2.png";
 import logo from "../../css/img/Logo.png";
 
-export const Card = () => {
+export const Card = ({ users }) => {
+  console.log("Card", users);
   return (
-    <div className={css.Wrapper}>
-      <img src={logo} alt="logo" className={css.logo} />
-      <img src={sigths} alt="sights" className={css.sigths} />
-      <img src={boy} alt="boy" className={css.boy} />
-      <div className={css.line}></div>
-      <p className={css.Tweets}>777 TWEETS</p>
-      <p className={css.Followers}>100,500 FOLLOWERS</p>
-      <button className={css.Button}>Follow</button>
-    </div>
+    <ul className={css.list}>
+      {users.map(({ id, avatar, name, followers, tweets }) => {
+        return (
+          <li key={id} className={css.item}>
+            <div className={css.Wrapper}>
+              <img src={logo} alt="logo" className={css.logo} />
+              <img src={sigths} alt="sights" className={css.sigths} />
+              <img src={boy} alt="boy" className={css.boy} />
+              <div className={css.line}></div>
+              <p className={css.Tweets}>{tweets} TWEETS</p>
+              <p className={css.Followers}>{followers} FOLLOWERS</p>
+              <button className={css.Button}>Follow</button>
+            </div>
+          </li>
+        );
+      })}
+    </ul>
   );
 };
--- src/pages/Users.jsx
+++ src/pages/Users.jsx
@@ -1,9 +1,21 @@
+import { useEffect, useState } from "react";
 import { Card } from "../components/Card/Card";
+import { getUsers } from "../service/operations";
 
 export const Users = () => {
+  const [resp, setResp] = useState([]);
+
+  useEffect(() => {
+    getUsers().then((resp) => setResp(resp));
+  }, []);
+
+  if (!resp) {
+    return null;
+  }
+
   return (
     <div>
-      <Card />
+      <Card users={resp} />
     </div>
   );
 };
