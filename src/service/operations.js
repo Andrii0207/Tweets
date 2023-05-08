a3c5492f967df0c76daa2f3ca853df9520a5dafd
Andrii <Popov.Andrey80@gmail.com>
2023-05-08 21:50:20 +0300

    Update operations.js

--- src/service/operations.js
+++ src/service/operations.js
@@ -4,5 +4,5 @@ axios.defaults.baseURL = "https://6457b87d1a4c152cf988b585.mockapi.io";
 
 export const getUsers = async () => {
   const { data } = await axios.get("/users");
-  return console.log(data);
+  return data;
 };
