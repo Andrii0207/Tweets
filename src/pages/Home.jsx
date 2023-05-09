ca3eb016055a62aee7d343fb5536f04debcf7224
Andrii <Popov.Andrey80@gmail.com>
2023-05-09 23:01:40 +0300

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,3 @@
-import { useEffect } from "react";
-import { getUsers } from "../service/operations";
-
 export const Home = () => {
-  useEffect(() => {
-    getUsers();
-  }, []);
-
   return <div>Welcome!</div>;
 };
